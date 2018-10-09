fe8bcdf63ce78b2676430bb5a431b2c8e0a1e33f
Juan Pablo Jou <jjouvalencia@deloitte.co.uk>
2018-10-09 17:32:33 +0100

    connect to db and create a sample entry

---
+++ create.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
+
+export async function main(event, context, callback) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: "contacts",
+    Item: {
+        email: event.requestContext.identity.cognitoIdentityId,
+    //   userId: event.requestContext.identity.cognitoIdentityId,
+    //   noteId: uuid.v1(),
+        content: data.content,
+        attachment: data.attachment,
+        createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    callback(null, success(params.Item));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
